e79a0406187f7c6007d10c915944cc4822ed559a
Richie Darylson Tandiono <richie.tandiono@binus.edu>
2024-02-26 09:23:34 +0700

    Search function

--- funcs.js
+++ funcs.js
@@ -8,18 +8,19 @@ var data = [
 ]
 
 var time = [1, 2, 3, 4, 5, 6]
-var val = [3,6,8,2,5,8]
+var val = [3, 6, 8, 2, 5, 8]
 
+const trend = new Trend()
 
-function draw(kind)
-{
+var selectedGraph = "select-line";
+var currQuery = "";
+
+function draw(kind, movie)
+{    
     $("#svg-graph").remove()
     const height = parseInt(d3.select("#graph").style("height"),10)
     const width = parseInt(d3.select("#graph").style("width"), 10)
     const margin = { v: height / 10, h: width / 10 }
-
-    const y = d3.scaleLinear().range([height - margin.v, 0]).domain([2,9])
-    const x = d3.scaleLinear().range([0, width - margin.h]).domain([1, 6])
     
     const svg = d3.select("#graph")
         .append("svg")
@@ -29,24 +30,31 @@ function draw(kind)
         .append("g")
             .attr("transform", `translate(${margin.h/2}, ${margin.v/2})`)
     
-    if (kind == "line") {
+    if (kind == "select-line") {
+        var data = trend.get_interest_over_time(movie)
+
+        var timeFormat = d3.timeFormat("%Y");
+
+        const y = d3.scaleLinear().range([height - margin.v, 0]).domain([Math.min(...data.map(d=>d[1])), Math.max(...data.map(d=>d[1]))])
+        const x = d3.scaleTime().range([0, width - margin.h]).domain([Math.min(...data.map(d=>d[0])), Math.max(...data.map(d=>d[0]))]).nice()
+
         svg.append("g")
-            .call(d3.axisBottom(x).ticks(10, ",f"))
+            .call(d3.axisBottom(x).ticks(10, timeFormat))
             .attr("transform", `translate(0, ${height - margin.v})`)
     
         svg.append("g")
             .call(d3.axisLeft(y))
         
-        const line = d3.line().x(d => x(d.time)).y(d => y(d.val))
+        const line = d3.line().x(d => x(d[0])).y(d => y(d[1]))
         
         svg.append("path")
             .datum(data)
             .attr("fill", "none")
             .attr("stroke", "steelblue")
-            .attr("stroke-width", 1)
+            .attr("stroke-width", 2)
             .attr("d", line)
     }
-    else if (kind == "map")
+    else if (kind == "select-map")
     {
         var projection = d3.geoNaturalEarth1().scale(width / (2 * Math.PI)).translate([width / 2.5, height / 2.5])
         var geo = JSON.parse(geoData)
@@ -66,14 +74,67 @@ function draw(kind)
         
 }
 
-window.addEventListener("resize", function () { draw("map") })
 
 function search()
 {
     var res = $("#search-bar").val();
-    alert(res)
+    if (trend.movie_exists(res))
+    {
+        currQuery = res
+        draw(selectedGraph, currQuery)
+    }
     $("#search-bar").val("")
 }
+
+
+function search_suggestions(string)
+{
+    var suggestion_list = $("#search-suggestion-list")
+    var suggestion = trend.match_string(string)
+    suggestion_list.empty()
+    for (let i = 0; i < suggestion.length;  i++)
+    {
+        suggestion_list.append(`<dt class="search-suggestion-item">${suggestion[i]}</dt>`)
+    }
+    $(".search-suggestion-item").on('click', (e) => {
+        suggestion_list.empty()
+        $("#search-bar").val($(e.target).text());
+        $("#search-button").click();
+    })
+}
+
+
+$(document).ready(() => {
+    window.addEventListener("resize", function () { draw(selectedGraph, trend.get_interest_over_time(currQuery)) })
+    window.addEventListener("load", () => { $("#search-bar").val("") })
+
+    $("#search-button").on("click", search);
+    $("#search-bar").on("input focus", (e) =>
+    {
+        search_suggestions($(e.target).val());
+    })
+
+    $("body").on('click', (e) =>
+    {
+        if (!($(e.target).is("#search-bar") || $(e.target).is("#search-suggestion-list")))
+        {
+            $("#search-suggestion-list").empty();
+        }
+    })
+
+    var selected = $("#" + selectedGraph);
+    selected.addClass("selected-tab");
+    
+    $(".select-tab").on('click', (e) =>
+    {
+        selected.removeClass("selected-tab")
+        selected = $(e.target)
+        selected.addClass("selected-tab")
+        selectedGraph = selected.attr("id");
+
+        draw(selectedGraph, currQuery)
+    })
+});
 
 
 /*
